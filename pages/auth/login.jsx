6e03a900727f6be736d3c28e27089575ea4ef3e1
kragleh <danidurisik@gmail.com>
2023-02-03 10:15:50 +0100

    Hot Fix

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -14,7 +14,7 @@ export default function Login() {
 
   return (
     <main className="h-screen w-screen overflow-hidden flex flex-col gap-4 justify-center items-center">
-      <h1 className="font-semibold text-2xl">Login To Continue</h1>
+      <h1 className="font-semibold text-2xl text-white">Login To Continue</h1>
       <form className="p-4 bg-white rounded-2xl flex flex-col gap-2">
         <input type="password" name="apikey" id="apikey" placeholder="Your Key" className="p-2 bg-blue-100 border-2 border-gray-300 rounded text-black text-center" />
         <button onClick={handleLogin} type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded duration-200">Login</button>
--- pages/auth/logout.jsx
+++ pages/auth/logout.jsx
@@ -1,22 +1,17 @@
 import { deleteCookie } from "cookies-next"
 import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 export default function Logout() {
 
   const router = useRouter()
 
-  function handleLogout() {
-    event.preventDefault()
+  useEffect(() => {
     deleteCookie('apikey')
     router.push('./login')
-  }
+  }) 
 
   return (
-    <main className="h-screen w-screen overflow-hidden flex flex-col gap-4 justify-center items-center">
-      <h1 className="font-semibold text-2xl">Are you sure?</h1>
-      <form className="p-4 bg-white rounded-2xl flex flex-col gap-2">
-        <button onClick={handleLogout} type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded duration-200">Log Out</button>
-      </form>
-    </main>
+    <h1 className="text-center text-white font-semibold text-2xl">Are you sure?</h1>
   )
 }
